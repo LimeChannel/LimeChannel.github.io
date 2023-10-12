9f5259ac99a91d4c764ae0532c169c694ddd9dd2
LimeChannel <LimeChann@yandex.ru>
2023-10-12 19:56:45 +0300

    Homeworl 5 raw 3

--- Homework 5/main.js
+++ Homework 5/main.js
@@ -1,10 +1,14 @@
-var result = document.getElementById('result')
-var choice = document.getElementById('choice')
-var count = document.getElementById('count')
+// var result = document.getElementById('result')
+// var choice = document.getElementById('choice')
+// var count = document.getElementById('count')
+var prices = {'0': 50, '1': 60, '2':70}
 
 function calculate(form)
 {
+    
     var amount = form.count.value
-    console.log(amount)
+    var type = form.type.value
+    console.log(prices[type] * amount)
+
     return false
 }
