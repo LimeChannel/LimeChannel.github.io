6557cbc026e390e378ad33b3a41ddf39b2641c3b
LimeChannel <LimeChann@yandex.ru>
2023-10-12 19:47:08 +0300

    Homework 5 raw 2

---
+++ Homework 5/main.js
@@ -0,0 +1,10 @@
+var result = document.getElementById('result')
+var choice = document.getElementById('choice')
+var count = document.getElementById('count')
+
+function calculate(form)
+{
+    var amount = form.count.value
+    console.log(amount)
+    return false
+}
