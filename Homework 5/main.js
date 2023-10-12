04fc4516dad8e392cfc0827e32d26eaeea51bd61
LimeChannel <LimeChann@yandex.ru>
2023-10-12 22:04:12 +0300

    Homework 5 done

--- Homework 5/main.js
+++ Homework 5/main.js
@@ -1,15 +1,20 @@
-// var result = document.getElementById('result')
-// var choice = document.getElementById('choice')
-// var count = document.getElementById('count')
-var prices = {'0': 50, '1': 60, '2':70}
+var prices = {'0': 50, '1': 100, '2':200}
 
 function calculate(form)
 {
-    
+    var regex = /^[0-9]+$/
     var amount = form.count.value
     var type = form.type.value
-    console.log(prices[type] * amount)
-    document.getElementById('result').innerText='Итог: ' + prices[type] * amount
+    console.log(form.count.value.match(regex))
+    if (amount.match(regex) == null)
+    {
+        console.log(form.count.value.match(regex))
+        alert('Недопустимые символы в поле')
+    }
+    else
+    {
+        document.getElementById('result').innerText='Итог: ' + prices[type] * amount
+    }
 
     return false
 }
