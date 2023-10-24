cef40acc281784e51f3993f1a7b00daa2cb2c357
LimeChannel <LimeChann@yandex.ru>
2023-10-24 16:21:08 +0300

    Homework 7 added

---
+++ Homework 7/main.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
